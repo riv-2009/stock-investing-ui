8819ec6cfa3d137d24e6e1e850b39ea2d55a1825
matt <mattwjames@msn.com>
2022-07-05 13:03:46 -0600

    "first commit"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Prompt from "./Prompt";
+import { useState, useEffect } from "react";
+import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState("Enter a ticker symbol");
+    const [input, setInput] = useState("");
+    const [connection, setConnection] = useState();
+
+    useEffect(() => {
+        const connection = new HubConnectionBuilder()
+            .withUrl("https://localhost:7097/stockHub")
+            .configureLogging(LogLevel.Information)
+            .build();
+
+        async function start() {
+            try {
+                await connection.start();
+                console.log("SignalR Connected.");
+            } catch (err) {
+                console.log(err);
+                setTimeout(start, 5000);
+            }
+        }
+
+        connection.onclose(async () => {
+            await start();
+        });
+
+        // Start the connection.
+        start();
+        setConnection(connection);
+    }, []);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        connection
+            .invoke("StockTickerMessage", input)
+            .catch((err) => console.error(err.toString()));
+        connection.on("ReceiveMessage", (input) => {
+            let tmp = JSON.parse(input);
+            console.log(tmp);
+            //only get 7 days of stock data,
+            //the number of day's returned varies depending if the stock market was open
+            for (let index = 0; index < 7; index++) {
+                console.log(tmp.results[index].o);
+            }
+        });
+    };
+
+    return (
+        <div className="container">
+            <Prompt
+                handleSubmit={handleSubmit}
+                input={input}
+                message={message}
+                setInput={setInput}
+            />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Prompt.js
@@ -0,0 +1,23 @@
+const Prompt = (props) => {
+
+    return (
+        <>
+            <form onSubmit={props.handleSubmit}>
+                <label>
+                    <h4>{props.message}</h4>
+                    <input
+                        type="text"
+                        className="form-control"
+                        value={props.input}
+                        onChange={(e) => {
+                            props.setInput(e.target.value);
+                        }}
+                    />
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </label>
+            </form>
+        </>
+    );
+};
+
+export default Prompt;
