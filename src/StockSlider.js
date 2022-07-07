6ce28378fc1cee171cfb1bde3a70d6e84bb234f8
DenverJensen <jensendenver@gmail.com>
2022-07-06 22:36:49 -0600

    navigation and slider update

--- src/StockSlider.js
+++ src/StockSlider.js
@@ -2,15 +2,33 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 
-const StockSlider = ({ shares}) => {
+const StockSlider = ({ sharesPurchased, shares, Value, setValue, Action }) => {
     return (
         <Box width={300}>
-            <Slider
-                defaultValue={0}
-                aria-label="Default"
-                valueLabelDisplay="auto"
-                max={Math.trunc(shares)}
-            />
+            {Action == "buy" && (
+                <Slider
+                    defaultValue={Value}
+                    aria-label="Default"
+                    valueLabelDisplay="auto"
+                    getAriaValueText={Value}
+
+                    max={Math.trunc(shares)}
+                    onChange={(e) => {
+                        setValue(e.target.value);
+                    }}
+                />
+            )}
+            {Action == "sell" && (
+                <Slider
+                defaultValue={Value}
+                    aria-label="Default"
+                    valueLabelDisplay="auto"
+                    max={Math.trunc(sharesPurchased)}
+                    onChange={(e) => {
+                        setValue(e.target.value);
+                    }}
+                />
+            )}
         </Box>
     );
 };
