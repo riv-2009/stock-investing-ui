b5d4dafe413c5f6b808b10f6d5be05dcacf233a0
matt <mattwjames@msn.com>
2022-07-30 23:05:35 -0600

    login

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState, useEffect } from 'react';
-import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
-import GameBoard from './wordBoard';
-import Login from './login';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
+import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
+import GameBoard from "./wordBoard";
+import Login from "./login";
 
 const App = () => {
     const [UserName, setUserName] = useState();
+    let player2 = "";
+    const [connection, setConnection] = useState("");
 
     useEffect(() => {
-        // const connection = new HubConnectionBuilder()
-        // 	.withUrl("https://localhost:7097/stockHub")
-        // 	.configureLogging(LogLevel.Information)
-        // 	.build();
-        // async function start() {
-        // 	try {
-        // 		await connection.start();
-        // 		console.log("SignalR Connected.");
-        // 	} catch (err) {
-        // 		console.log(err);
-        // 		setTimeout(start, 5000);
-        // 	}
-        // }
-        // connection.onclose(async () => {
-        // 	await start();
-        // });
-        // // Start the connection.
-        // start();
-        // setConnection(connection);
-        //check if user logged in, if not, push to login
+        const connection = new HubConnectionBuilder()
+            .withUrl("https://localhost:7097/wordHub")
+            .configureLogging(LogLevel.Information)
+            .build();
+        async function start() {
+            try {
+                await connection.start();
+                console.log("SignalR Connected.");
+            } catch (err) {
+                console.log(err);
+                setTimeout(start, 5000);
+            }
+        }
+        connection.onclose(async () => {
+            await start();
+        });
+        // Start the connection.
+        start();
+        setConnection(connection);
+        
     }, []);
 
     return UserName ? (
@@ -37,7 +39,7 @@ const App = () => {
             <GameBoard />
         </>
     ) : (
-        <Login/>
+        <Login setUserName={setUserName} connection={connection} />
     );
 };
 
--- src/login.js
+++ src/login.js
@@ -1,5 +1,77 @@
-const Login = () => {
-    return ( <>login system here</> );
-}
- 
+import { Input, Button, ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { useState } from "react";
+
+const Login = (props) => {
+    const [player, setPlayer] = useState();
+    const [player1Taken, setPlayer1Taken] = useState(false);
+    const [player2Taken, setPlayer2Taken] = useState(false);
+    const [userName, setUserName] = useState("");
+    const [msg, setMsg] = useState("");
+
+    const handleChange = (e) => {
+        updatePlayer();
+        setPlayer(e.target.value);
+    };
+
+    const updatePlayer = (e) => {
+        setPlayer1Taken(true);
+    }
+
+    const handleStartGame = (e) => {
+        if (player !== "" && userName !== "") {
+            props.setUserName(userName);
+            props.connection
+                .invoke("PlayerTaken", player)
+                .catch((err) => console.error(err.toString()));
+            props.connection.on("PlayerTakenMessage", (player) => {
+                setMsg(player);
+                updatePlayer();
+                console.log(player);
+            });
+        } else setMsg("Enter a username");
+    };
+    return (
+        <>
+            <div className="container">
+                <h1 className="title">Grid Word Finder</h1>
+                <h6>Enter a username</h6>
+                <Input
+                    variant="outlined"
+                    type="text"
+                    className="word-input"
+                    value={userName}
+                    onChange={(e) => {
+                        setUserName(e.target.value);
+                    }}
+                />
+                <div className="word-submit mt-4">
+                    <ToggleButtonGroup
+                        color="primary"
+                        value={player}
+                        exclusive
+                        onChange={handleChange}
+                    >
+                        <ToggleButton value="player1" disabled={player1Taken}>
+                            Player 1
+                        </ToggleButton>
+
+                        <ToggleButton value="player2" disabled={player2Taken}>
+                            PLayer 2
+                        </ToggleButton>
+                    </ToggleButtonGroup>
+                    <br />
+                    <Button
+                        className="options mt-4"
+                        variant="contained"
+                        onClick={handleStartGame}
+                    >
+                        start game
+                    </Button>
+                    <h6 className="text-danger mt-4">{msg}</h6>
+                </div>
+            </div>
+        </>
+    );
+};
+
 export default Login;
