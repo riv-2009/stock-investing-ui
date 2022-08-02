17f14d1f3b4735daeb978f3dd865f35a53dd8d77
DenverJensen <jensendenver@gmail.com>
2022-08-01 19:09:30 -0600

    game board updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 import GameBoard from './wordBoard/wordBoard';
 import Login from './login';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const App = () => {
     const [connection, setConnection] = useState('');
@@ -33,36 +34,37 @@ const App = () => {
     }, []);
 
     useEffect(() => {
-        const connection = new HubConnectionBuilder()
-            .withUrl('https://localhost:7097/wordHub')
-            .configureLogging(LogLevel.Information)
-            .build();
-        async function start() {
-            try {
-                await connection.start();
-                console.log('SignalR Connected.');
-            } catch (err) {
-                console.log(err);
-                setTimeout(start, 5000);
-            }
-        }
-        connection.onclose(async () => {
-            await start();
-        });
-        // Start the connection.
-        start();
-
-        setConnection(connection);
+        // const connection = new HubConnectionBuilder()
+        //     .withUrl('https://localhost:7097/wordHub')
+        //     .configureLogging(LogLevel.Information)
+        //     .build();
+        // async function start() {
+        //     try {
+        //         await connection.start();
+        //         console.log('SignalR Connected.');
+        //     } catch (err) {
+        //         console.log(err);
+        //         setTimeout(start, 5000);
+        //     }
+        // }
+        // connection.onclose(async () => {
+        //     await start();
+        // });
+        // // Start the connection.
+        // start();
+        // setConnection(connection);
     }, []);
 
     //replace true with player2 so that gamebaord shows when players are logged in
-    if (true)
-        return (
+
+    return (
+        <ChakraProvider>
             <div className="game-container">
                 {/* add game items here */}
                 <GameBoard Letters={Letters} />
             </div>
-        );
+        </ChakraProvider>
+    );
 };
 
 export default App;
--- src/wordBoard/wordBoard.js
+++ src/wordBoard/wordBoard.js
@@ -1,30 +1,100 @@
 import { useEffect, useState } from 'react';
 import './wordBoard.css';
+import { Box, Center, Spinner } from '@chakra-ui/react';
 
 const GameBoard = ({ Letters }) => {
     const [SelectedTiles, setSelectedTiles] = useState([]);
-    const [CurrentTile, setCurrentTile] = useState();
-
-
-
+    const [CurrentTile, setCurrentTile] = useState({
+        letter: null,
+        TileIndex: null,
+    });
+    const [AvailableTiles, setAvailableTiles] = useState([]);
+    const [Style, setStyle] = useState('Blue');
+    const [Message, setMessage] = useState();
 
     useEffect(() => {
-        console.log(Letters);
-    }, [Letters]);
+        setTiles();
+    }, [CurrentTile]);
 
     const handleTileClick = (letter, index) => {
-        setCurrentTile(letter, index)
+        setCurrentTile({ letter, TileIndex: index });
+        setSelectedTiles([index, ...SelectedTiles]);
     };
 
-    const availableTiles = () =>{
-        return 
-    }
+    const setTiles = () => {
+        switch (CurrentTile.TileIndex) {
+            case 0:
+                setAvailableTiles([1, 4, 5]);
+                break;
+            case 1:
+                setAvailableTiles([0, 1, 4, 5, 6, 2]);
+                break;
+            case 2:
+                setAvailableTiles([1, 5, 6, 7, 3]);
+                break;
+            case 3:
+                setAvailableTiles([2, 6, 7]);
+                break;
+            case 4:
+                setAvailableTiles([0, 1, 5, 8, 9]);
+                break;
+            case 5:
+                setAvailableTiles([0, 1, 2, 6, 10, 9, 8, 4]);
+                break;
+            case 6:
+                setAvailableTiles([1, 2, 3, 5, 7, 9, 10, 11]);
+                break;
+            case 7:
+                setAvailableTiles([3, 2, 6, 10, 11]);
+                break;
+            case 8:
+                setAvailableTiles([4, 5, 9, 12, 13]);
+                break;
+            case 9:
+                setAvailableTiles([4, 5, 6, 8, 10, 12, 13, 14]);
+                break;
+            case 10:
+                setAvailableTiles([5, 6, 7, 9, 11, 13, 14, 15]);
+                break;
+            case 11:
+                setAvailableTiles([6, 7, 10, 14, 15]);
+                break;
+            case 12:
+                setAvailableTiles([8, 9, 13]);
+                break;
+            case 13:
+                setAvailableTiles([8, 9, 10, 12, 14]);
+                break;
+            case 14:
+                setAvailableTiles([13, 9, 10, 11, 15]);
+                break;
+            case 15:
+                setAvailableTiles([14, 10, 11]);
+                break;
+            default:
+                break;
+        }
+        return;
+    };
+
+
+    const colorStyle = (index) => {
+        for(let i in AvailableTiles){
+            if (AvailableTiles[i] === index){
+                return 'lightblue'
+            }
+        }
+        if (index > -1) {
+            return 'lightgreen';
+        }
+    };
 
     return Letters ? (
-        <div className="board-container">
+        <Box className="board-container">
             {Letters.map((letter, index) => (
-                <div
-                    className="board-tile"
+                <Box
+                    className={'board-tile '}
+                    backgroundColor={colorStyle(index)}
                     onClick={() => {
                         handleTileClick(letter, index);
                     }}
@@ -33,11 +103,17 @@ const GameBoard = ({ Letters }) => {
                     color={'green'}
                 >
                     {letter}-{index}
-                </div>
+                </Box>
             ))}
-        </div>
+            <Center>{Message}</Center>
+            {Message}
+        </Box>
     ) : (
-        <>Loading</>
+        <Center>
+            <h1>
+                <Spinner size="lg">Loading...</Spinner>
+            </h1>
+        </Center>
     );
 };
 
